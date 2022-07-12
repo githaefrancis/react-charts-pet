95f188aeebc85a65d09d518634f282022ad39777
Francis Githae <mureithigithae@gmail.com>
2022-07-12 18:47:25 +0300

    setup initial project

---
+++ src/components/flowchart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ReactDOM } from 'react-dom';
+import { Flowchart } from '@ant-design/charts';
+const SampleFlowChart = () =>{
+
+    return (
+
+        <>
+        
+        </>
+    );
+};
+
+export default SampleFlowChart
